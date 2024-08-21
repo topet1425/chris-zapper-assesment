b141a5473223ce3012cb7de1b8733026aab76e4e
topet1425 <topetmarqueses25@gmail.com>
2024-08-21 12:56:48 +0800

    Initial upload

---
+++ ZapperAssesment/ZapperAssesment-question-1.sql
@@ -0,0 +1,64 @@
+USE zapperdb
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Customers]') AND type = N'U')
+BEGIN
+	CREATE TABLE Customers (
+		Id INT PRIMARY KEY IDENTITY(1,1),
+		FirstName VARCHAR(50) NOT NULL,
+		LastName VARCHAR(50) NOT NULL,
+		Email VARCHAR(100) UNIQUE NOT NULL,
+		PhoneNumber VARCHAR(20),
+		CreatedAt DATETIME DEFAULT GETDATE()
+	)
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Merchants]') AND type = N'U')
+BEGIN
+	CREATE TABLE Merchants (
+		Id INT PRIMARY KEY IDENTITY(1,1),
+		MerchantName VARCHAR(100) NOT NULL,
+		ContactEmail VARCHAR(100) UNIQUE NOT NULL,
+		PhoneNumber VARCHAR(20),
+		Address VARCHAR(255),
+		CreatedAt DATETIME DEFAULT GETDATE()
+	);
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Transactions]') AND type = N'U')
+BEGIN
+	CREATE TABLE Transactions (
+		Id INT PRIMARY KEY IDENTITY(1,1),
+		CustomerId INT NOT NULL,
+		MerchantId INT NOT NULL,
+		Amount DECIMAL(18, 2) NOT NULL,
+		CreatedAt DATETIME DEFAULT GETDATE(),
+		PaymentMethod VARCHAR(50) NOT NULL,
+		Status VARCHAR(20) NOT NULL CHECK (Status IN ('Pending', 'Completed', 'Failed', 'Refunded')),
+		FOREIGN KEY (CustomerId) REFERENCES Customers(Id),
+		FOREIGN KEY (MerchantId) REFERENCES Merchants(Id)
+	);
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[TransactionItems]') AND type = N'U')
+BEGIN
+	CREATE TABLE TransactionItems (
+		Id INT PRIMARY KEY IDENTITY(1,1),
+		TransactionId INT NOT NULL,
+		ItemDescription VARCHAR(255) NOT NULL,
+		Quantity INT NOT NULL,
+		UnitPrice DECIMAL(18, 2) NOT NULL,
+		FOREIGN KEY (TransactionId) REFERENCES Transactions(Id)
+	);
+END
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Payments]') AND type = N'U')
+BEGIN
+	CREATE TABLE Payments (
+		Id INT PRIMARY KEY IDENTITY(1,1),
+		TransactionId INT NOT NULL,
+		PaymentAmount DECIMAL(18, 2) NOT NULL,
+		PaymentDate DATETIME DEFAULT GETDATE(),
+		PaymentStatus VARCHAR(20) NOT NULL CHECK (PaymentStatus IN ('Paid', 'Unpaid', 'Refunded')),
+		ApprovalCode VARCHAR(10) NULL
+		FOREIGN KEY (TransactionId) REFERENCES Transactions(Id)
+	);
+END
---
+++ ZapperAssesment/ZapperAssesment/Program.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //Ucomment to test Question 2.1
+        /*
+        Console.Write("Enter user settings: ");
+        string settings = Console.ReadLine();
+
+        Console.Write("Enter setting (1-8): ");
+        string setting = Console.ReadLine();
+
+        if (int.TryParse(setting, out int value))
+        {
+            Console.WriteLine(IsFeatureEnabled(settings, value));
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException(nameof(setting), "Setting is invalid.");
+        }
+        */
+       
+
+        //Uncomment to test Question 2.2
+        /*
+        WriteSettingsToFile("userSettings.txt", "00000010");
+        string settings = ReadSettingsFromFile("userSettings.txt");
+        Console.WriteLine(settings); 
+        */
+
+    }
+
+    static bool IsFeatureEnabled(string settings, int setting)
+    {
+        if (setting < 1 || setting > settings.Length)
+        {
+            throw new ArgumentOutOfRangeException(nameof(setting), "User setting is out of range.");
+        }
+
+        return settings[setting -1] == '1';
+    }
+
+    static void WriteSettingsToFile(string filePath, string settings)
+    {
+        File.WriteAllText(filePath, settings);
+    }
+
+    static string ReadSettingsFromFile(string filePath)
+    {
+        return File.ReadAllText(filePath).Trim();
+    }
+}
